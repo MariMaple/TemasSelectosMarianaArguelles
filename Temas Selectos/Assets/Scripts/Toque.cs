fde81d65634c6191c0b401dddacd83c9bb0a94b9
Mariana Arguelles <54082872+MariMaple@users.noreply.github.com>
2021-10-11 16:44:06 -0500

    Practica 2 V1

---
+++ Temas Selectos/Assets/Scripts/Toque.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toque : MonoBehaviour
+{
+    public Texture[] texture;
+    public int currentTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touches[0].phase == TouchPhase.Began)
+        {
+            currentTexture++;
+            currentTexture %= texture.Length;
+            gameObject.GetComponent<Renderer>().material.mainTexture = texture[currentTexture];
+        }
+    }
+}
