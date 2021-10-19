b1ce573862ef5f6699c591e7677c5e9d68af18dd
Mariana Arguelles <54082872+MariMaple@users.noreply.github.com>
2021-10-19 17:50:20 -0500

    Practica 4

---
+++ Temas Selectos/Assets/Scenes/Scripts/ChangeScenes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenes : MonoBehaviour
+{
+    //funcion que carga la escena 
+    public void Load_Scene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
