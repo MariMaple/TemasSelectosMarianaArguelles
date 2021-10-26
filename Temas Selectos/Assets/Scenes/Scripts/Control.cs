35158645b99bc88ecb01f2bc3b928ab34ab2fef4
Mariana Arguelles <54082872+MariMaple@users.noreply.github.com>
2021-10-26 14:18:21 -0500

    practica 5
    Practica 5

---
+++ Temas Selectos/Assets/Scenes/Scripts/Control.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public float velocidad = 6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)||Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * velocidad * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * velocidad * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
+        {
+            transform.position += Vector3.forward * velocidad * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
+        {
+            transform.position += Vector3.back * velocidad * Time.deltaTime;
+        }
+    }
+}
---
+++ Temas Selectos/Assets/Scenes/Scripts/Reloj.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reloj : MonoBehaviour
+{
+    private float IniTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IniTimer = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Control;
+        string Min;
+        string Seg;
+        string RelojText;
+
+        Control = Time.time - IniTimer;
+        Min = ((int)Control / 60).ToString("00");
+        Seg = (Control % 60).ToString("00");
+        RelojText = string.Format("{00}:{01}", Min, Seg);
+        GetComponent<Text>().text = RelojText.ToString();
+    }
+}
