63f714bb3ca09567926b4be1299c7ef4b8f4cf4d
Mariana Arguelles <54082872+MariMaple@users.noreply.github.com>
2021-11-09 00:07:50 -0600

    Practica 7

---
+++ Temas Selectos/Assets/Scenes/Scripts/AnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{   public Animator AnimController;
+    public AudioSource audioPersonaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            AnimController.SetBool("avanza", true);
+            AnimController.SetBool("ruge", true);
+        }
+        if(AnimController.GetCurrentAnimatorClipInfo(0)[0].clip.name=="walking"|| AnimController.GetCurrentAnimatorClipInfo(0)[0].clip.name == "roar")
+        {
+            AnimController.SetBool("avanza", false);
+            AnimController.SetBool("ruge", false);
+            if (!audioPersonaje.isPlaying)
+            {
+                audioPersonaje.Play();
+            }
+        }
+        if (AnimController.GetCurrentAnimatorClipInfo(0)[0].clip.name == "idle")
+        {
+            audioPersonaje.Pause();
+        }
+    }
+}
