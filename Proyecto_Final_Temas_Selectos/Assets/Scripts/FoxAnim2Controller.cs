97cea59f167af211d240f704844c66fe059a6335
Mariana Arguelles <54082872+MariMaple@users.noreply.github.com>
2021-12-08 07:01:10 -0600

    implementacion de zorros y diferentes animaciones

--- Proyecto_Final_Temas_Selectos/Assets/Scripts/FoxAnim2Controller.cs
+++ Proyecto_Final_Temas_Selectos/Assets/Scripts/FoxAnim2Controller.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoxAnimController : MonoBehaviour
+public class FoxAnim2Controller : MonoBehaviour
 {
-    public float speed = 1f;
-    public int randomize = 0;
-    public int mov = 0;
+
     public Animator AnimController;
     // Start is called before the first frame update
     void Start()
@@ -16,23 +14,15 @@ public class FoxAnimController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        AnimController.SetInteger("state", 2);
-        Vector3 movInput = Vector3.zero;
-        movInput.x = .001f;
-        if (mov==0)
-        {
-            transform.position += movInput.normalized * speed * Time.deltaTime;
-        }
+        AnimController.SetInteger("state", 1);
     }
 
     void OnTriggerEnter(Collider obj)
     {
         if (obj.tag == "fruit")
         {
-            mov = 1;
             AnimController.SetInteger("state", 1);
             Destroy(obj.gameObject);
-            yield WaitForSeconds(6);
         }
     }
 }
