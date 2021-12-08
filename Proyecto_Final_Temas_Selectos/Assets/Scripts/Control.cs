7830107b217a05ca3f65b39e317eb354d521fb3c
Mariana Arguelles <54082872+MariMaple@users.noreply.github.com>
2021-12-08 01:52:25 -0600

    Modelos implementados
    Esto es antes de las animaciones,

---
+++ Proyecto_Final_Temas_Selectos/Assets/Scripts/Control.cs
@@ -0,0 +1,63 @@
+﻿
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public int active = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
+        {
+            Vector3 movInput = Vector3.zero;
+            movInput.x = -1;
+            transform.position += movInput.normalized * speed * Time.deltaTime;
+            active = 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
+        {
+            Vector3 movInput = Vector3.zero;
+            movInput.x = 1;
+            transform.position += movInput.normalized * speed * Time.deltaTime;
+            active = 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey("w"))
+        {
+            Vector3 movInput = Vector3.zero;
+            movInput.z = 1;
+            transform.position += movInput.normalized * speed * Time.deltaTime;
+            active = 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey("s"))
+        {
+            Vector3 movInput = Vector3.zero;
+            movInput.z = -1;
+            transform.position += movInput.normalized * speed * Time.deltaTime;
+            active = 1;
+        }
+        else
+        {
+            active = 0;
+        }
+        //if (Input.GetKey("q"))
+        //{
+        //    transform.Rotate(new Vector3(0f, -90f, 0f) * Time.deltaTime);
+        //}
+        //if (Input.GetKey("e"))
+        //{
+        //    transform.Rotate(new Vector3(0f, 90f, 0f) * Time.deltaTime);
+        //}
+    }
+  
+}
